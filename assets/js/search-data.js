ba29077cf527bc3b0f7ad8669f12c6292c1fc1c5
JacquelineRowe <jacquierowe95@gmail.com>
2025-01-03 09:36:33 +0000

    Deploying to gh-pages from @ JacquelineRowe/jacquelinerowe.github.io@8681298b6f8b6dc5024f1f586b2ccbab5d6219a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-commentary",
+          title: "Commentary",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/commentary/index.html";
+          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
